0a38a96bdc87178fb2e2fab077fb7ab53ab5c3a1
Sylvia702 <heqilve@foxmail.com>
2025-03-03 21:29:23 +0800

    Update SliceOHeaven.java

--- SliceOHeaven.java
+++ SliceOHeaven.java
@@ -13,6 +13,7 @@ public class SliceOHeaven {
     public static final String DEF_ORDER_ID = "DEF-SOH-099";
     public static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
     public static final double DEF_ORDER_TOTAL = 15.00;
+
     public SliceOHeaven() {
         this.orderID = DEF_ORDER_ID;
         this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
@@ -20,23 +21,29 @@ public class SliceOHeaven {
         this.sides = "";
         this.drinks = "";
     }
+
     public SliceOHeaven(String orderID, String pizzaIngredients, double orderTotal) {
         this.orderID = orderID;
         this.pizzaIngredients = pizzaIngredients;
         this.orderTotal = orderTotal;
     }
+
     public String getOrderID() {
         return orderID;
     }
+
     public void setOrderID(String orderID) {
         this.orderID = orderID;
     }
+
     public double getOrderTotal() {
         return orderTotal;
     }
+
     public void setOrderTotal(double orderTotal) {
         this.orderTotal = orderTotal;
     }
+
     public void takeOrder() {
         System.out.println("Taking order...");
     }
@@ -44,7 +51,38 @@ public class SliceOHeaven {
     public void makePizza() {
         System.out.println("Making pizza...");
     }
+
     private void printReceipt() {
         System.out.println("Printing receipt...");
+    }
+
+    public void processCardPayment(String cardNumber, String expiryDate, int cvv) {
+        int cardLength = cardNumber.length();
+        if (cardLength == 14) {
+            System.out.println("Card accepted");
+        } else {
+            System.out.println("Invalid card");
+        }
+
+        int firstCardDigit = Integer.parseInt(cardNumber.substring(0, 1));
+
+        String blacklistedNumber = "12345678901234";
+        if (cardNumber.equals(blacklistedNumber)) {
+            System.out.println("Card is blacklisted. Please use another card");
+        }
+
+        int lastFourDigits = Integer.parseInt(cardNumber.substring(cardNumber.length() - 4));
+
+        String cardNumberToDisplay = cardNumber.charAt(0) + 
+                                     cardNumber.substring(1, cardNumber.length() - 4).replaceAll(".", "*") + 
+                                     cardNumber.substring(cardNumber.length() - 4);
+    }
+
+    public void specialOfTheDay(String pizzaOfTheDay, String sideOfTheDay, String specialPrice) {
+        StringBuilder specialInfo = new StringBuilder();
+        specialInfo.append("pizzzaOfTheDay - ").append(pizzaOfTheDay)
+                  .append(", sideOfTheDay - ").append(sideOfTheDay)
+                  .append(", specialPrice - ").append(specialPrice);
+        System.out.println(specialInfo.toString());
     }
 }
